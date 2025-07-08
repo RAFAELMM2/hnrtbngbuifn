c1b3c75d25dc35f4ec7daddf27f95fe98b2f6efb
RAFAELMM2 <Perigop038@gmail.com>
2025-07-08 19:07:20 -0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC7qg6OppLHwRjni_ZX5HQ7Q5lbIy0rwyc",
+  authDomain: "meu-jogo-multiplayer-ceae7.firebaseapp.com",
+  databaseURL: "https://meu-jogo-multiplayer-ceae7-default-rtdb.firebaseio.com",
+  projectId: "meu-jogo-multiplayer-ceae7",
+  storageBucket: "meu-jogo-multiplayer-ceae7.firebasestorage.app",
+  messagingSenderId: "86688143652",
+  appId: "1:86688143652:web:f0a50adf1b2c3509687bb1",
+  measurementId: "G-R1W7EY3LXY"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
